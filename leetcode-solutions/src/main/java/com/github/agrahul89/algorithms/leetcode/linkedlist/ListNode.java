139ab473cd109b45f7fc8e1b2eb360a80b5fedc7
RAHUL AGARWAL <rahul89.agarwal@gmail.com>
2024-11-15 21:54:40 +0530

    leetcode-easy-21

---
+++ leetcode-solutions/src/main/java/com/github/agrahul89/algorithms/leetcode/linkedlist/ListNode.java
@@ -0,0 +1,31 @@
+package com.github.agrahul89.algorithms.leetcode.linkedlist;
+
+public class ListNode {
+  int val;
+  ListNode next;
+
+  ListNode() {
+  }
+
+  ListNode(int val) {
+    this.val = val;
+  }
+
+  ListNode(int val, ListNode next) {
+    this.val = val;
+    this.next = next;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder builder = new StringBuilder("[ ");
+    ListNode next = this;
+    while (next != null) {
+      builder.append(next.val);
+      next = next.next;
+      if (next != null)
+        builder.append(", ");
+    }
+    return builder.append(" ]").toString();
+  }
+}
---
+++ leetcode-solutions/src/test/java/com/github/agrahul89/algorithms/leetcode/linkedlist/MergeTwoSortedListTest.java
@@ -0,0 +1,23 @@
+package com.github.agrahul89.algorithms.leetcode.linkedlist;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class MergeTwoSortedListTest {
+
+  @Test
+  void testMergeTwoLists() {
+    ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))));
+    ListNode list2 = new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))));
+    ListNode mergedList = new MergeTwoSortedList().mergeTwoLists(list1, list2);
+
+    ListNode temp = mergedList;
+
+    for (int value : new int[] { 1, 2, 3, 3, 4, 4, 5, 6 }) {
+      assertThat(temp.val).isEqualTo(value);
+      temp = temp.next;
+    }
+  }
+
+}
